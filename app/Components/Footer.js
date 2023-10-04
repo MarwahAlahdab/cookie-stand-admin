2283b410d4f6e0df8d07f1ed2e4ed2980d4f616e
marwasyr <marwaalahdab14@gmail.com>
2023-10-04 10:23:41 +0300

    lab37

---
+++ app/Components/Footer.js
@@ -0,0 +1,22 @@
+
+function Footer(){
+
+return(
+
+<footer className="bg-indigo-600 mt-16 text-white py-4 text-center">
+<p>&copy; 2023 Cookie Stand Admin</p>
+  <p>Contact: admin@cookiestand.com</p>
+  <p>Address: 123 Cookie Street, Cookieville</p>
+
+
+
+</footer>
+
+
+
+)
+
+}
+
+
+export default Footer;
---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+
+import CookieStandForm from '../app/components/CookieStandForm';
+
+
+export default function Home() {
+  return (
+  <div>
+  
+
+   <main>
+        <CookieStandForm />
+      
+      </main>
+  </div>
+   
+  )
+}
+
+
